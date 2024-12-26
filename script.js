b344730e5512221c854d9d6a8d91bd255540fdc2
Zahid Hasan <zhasan@ualberta.ca>
2024-12-25 22:45:18 -0700

    fixed errors

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-=const video = document.getElementById('video');
+const video = document.getElementById('video');
 const knownFaceDescriptors = [];
 const knownFaceNames = [];
 let showExpressions = false;
